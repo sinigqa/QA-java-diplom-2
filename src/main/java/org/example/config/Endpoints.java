d54824dce579cc69d79060529d44eade89febb3c
Igor Pavlov <sinigamimasters@gmail.com>
2025-08-08 12:20:02 +0300

    Обновил pom.xml создал несолько классов для настроек

---
+++ src/main/java/org/example/config/Endpoints.java
@@ -0,0 +1,12 @@
+package org.example.config;
+
+public class Endpoints {
+    public static class User {
+        public static final String CREATE = "/api/auth/register";
+        public static final String LOGIN = "/api/auth/login";
+    }
+
+    public static class Order {
+
+    }
+}
---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,18 @@
+package org.example.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class User {
+    private String email;
+    private String password;
+    private String userName;
+}
